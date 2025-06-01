67f3969414381d17ed191cb2a92c1c8b9dd34f6a
sankhare007 <dig1t_gost@gmail.com>
2025-06-01 15:19:43 +0000

    CSS modification

--- about/about.js
+++ about/about.js
@@ -18,3 +18,4 @@ const popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-tog
 popoverTriggerList.map(function (popoverTriggerEl) {
   return new bootstrap.Popover(popoverTriggerEl)
 })
+
